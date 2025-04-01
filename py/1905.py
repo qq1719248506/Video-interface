e2399d7529d3742e8e4063c6d3abb62b401aa628
qq1719248506 <119930866+qq1719248506@users.noreply.github.com>
2025-04-01 10:10:25 +0800

    Add files via upload

--- py/1905.py
+++ py/1905.py
@@ -4,16 +4,15 @@ import sys
 sys.path.append('..') 
 from base.spider import Spider
 import json
-import base64
 import re
-import math
 import time
 import uuid
 import hashlib
 from urllib import request, parse
 import urllib
 import urllib.request
-
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context#全局取消证书验证
 class Spider(Spider):  # 元类 默认的元类 type
 	def getName(self):
 		return "1905电影"
@@ -59,12 +58,11 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def categoryContent(self,tid,pg,filter,extend):
 		result = {}
 		videos=[]
-		by='/o1p'
+		by='/o3p'
 		if 'by' in extend.keys():
 			by='/{0}p'.format(extend['by'])
 		url = 'https://www.1905.com/vod/list/{0}{2}{1}.html'.format(tid, pg,by)
-		rsp = self.fetch(url, headers=self.header)
-		HtmlTxt=rsp.text
+		HtmlTxt =self.custom_webReadFile(url,header=self.header)
 		html = self.html(HtmlTxt)
 		aList = html.xpath("//section[contains(@class,'search-list')]/div/a" if tid != u'n_2' else "//div[@class='mod']/div[1]/a")
 		videos = self.custom_list(aList=aList)
@@ -77,54 +75,62 @@ class Spider(Spider):  # 元类 默认的元类 type
 		return result
 	def detailContent(self,array):
 		result = {}
-		aid = array[0]
+		temporary = array[0].split('###')
+		title=temporary[1]
+		aid=temporary[0]
+		pic=temporary[2]
+		remark=''
+		actor=''
+		direct=''
+		content=''
 		vodItems=[]
 		playList=[]
-		vod_play_from=['1905电影',]
-		if aid.isdigit()==False:
-			rsp = self.fetch(aid, headers=self.header)
-			HtmlTxt=rsp.text
-			url=self.regStr(reg=r'<a class="iconBanner-playBtn icon-banner btn-play"\s*href="(.+?)"',src=HtmlTxt)
+		vod_play_from=['播放线路',]
+		if aid.isdigit()==False:			
+			HtmlTxt=self.custom_webReadFile(aid,self.header)
+			url=self.custom_RegexGetText(HtmlTxt,r'<a class="iconBanner-playBtn icon-banner btn-play"\s*href="(.+?)"',1)
 			if url=='':
-				url=self.regStr(reg=r'property="og:url"\scontent="(.+?)"',src=HtmlTxt)
-			if len(self.regStr(reg=r'(vip.1905)',src=url))>3:
-				vod_play_from=['1905电影(需要vip解析)',]
+				url=self.custom_RegexGetText(HtmlTxt,r'property="og:url"\scontent="(.+?)"', 1)
+			if self.custom_RegexGetText(url,r'/(film)/',1)!='':
+				HtmlTxt=self.custom_webReadFile(aid+'video',self.header)
+				url=self.custom_RegexGetText(HtmlTxt,r'<li class="video-position-icon\s{0,1}">\r*\n*\s*<a href="(.+?)"\s{1,4}class="online-list-positive other-vedio-url"', 1)
+			if len(self.custom_RegexGetText(url,r'(vip.1905)',1))>3:
+				vod_play_from=['播放线路(需要vip解析)',]
 				aid=url
+				title=self.custom_RegexGetText(HtmlTxt,r'<div class="container-right">\s*\r*\n*\t*<h1>(.+?)<',1).replace(' ','')
+				pic=self.custom_RegexGetText(HtmlTxt,r'<img class="poster" src="(.+?)"',1)
+				content=self.custom_RegexGetText(HtmlTxt,r'<p>(.+?)</p>',1)
+				vodItems.append(title + "$" + aid)
+				joinStr = '#'.join(vodItems)
+				playList.append(joinStr)
 			else:
-				aid=self.regStr(reg=r'play/(.*?)\.sh',src=url)
+				aid=self.custom_RegexGetText(url,r'play/(.*?)\.sh',1)
 		if aid=='':
 			return  {'list': []}
 		elif  aid.isdigit() and vod_play_from[0].find('需要vip解析')<0:
 			url = "https://www.1905.com/api/content/?callback=&m=Vod&a=getVodSidebar&id={0}&fomat=json".format(aid)
-			rsp = self.fetch(url, headers=self.header)
-			HtmlTxt=rsp.text
-			root = json.loads(HtmlTxt)
-			title = root['title']
-			pic = root['thumb']
-			remark = root['commendreason']
-			content = root['description']
-			actor = root['starring']
-			direct = root['direct']
-			vodItems.append(title + "$" + aid)
-			series = root['info']['series_data']
-			series = root['info']['series_data']
-			for ser in series:
-				vodItems.append(ser['title'] + "$" + ser['contentid'])
-			joinStr = '#'.join(vodItems)
-			playList.append(joinStr)
+			try:
+				HtmlTxt=self.custom_webReadFile(url,self.header)
+				root = json.loads(HtmlTxt)
+				title = root['title']
+				pic = root['thumb']
+				remark = root['commendreason']
+				content = root['description']
+				actor = root['starring']
+				direct = root['direct']
+				vodItems.append(title + "$" + aid)
+				series = root['info']['series_data']
+				series = root['info']['series_data']
+				for ser in series:
+					vodItems.append(ser['title'] + "$" + ser['contentid'])
+				joinStr = '#'.join(vodItems)
+				playList.append(joinStr)
+			except:
+				joinStr = '#'.join([title + "$" + aid])
+				playList.append(joinStr)
 		else:
-			rsp = self.fetch(array[0], headers=self.header)
-			HtmlTxt=rsp.text
-			title=self.regStr(reg=r'<div class="container-right">\s*\r*\n*\t*<h1>(.+?)<',src=HtmlTxt).replace(' ','')
-			pic=self.regStr(reg=r'<img class="poster" src="(.+?)"',src=HtmlTxt)
-			remark=''
-			actor=''
-			direct=''
-			content=self.regStr(reg=r'<p>(.+?)</p>',src=HtmlTxt)
-			vodItems.append(title + "$" + aid)
-			joinStr = '#'.join(vodItems)
-			playList.append(joinStr)
-
+			pass
+			
 		vod = {
 			"vod_id":array[0],
 			"vod_name":title,
@@ -147,12 +153,11 @@ class Spider(Spider):  # 元类 默认的元类 type
 		return result
 
 	def searchContent(self,key,quick):
-		url = 'https://www.1905.com/search/index-p-type-film-q-{}.html?envod=1&year=0&score=0&order=0'.format(key)#只搜索能看电影,想搜其它的可以把html之后的字符删掉
+		url = 'https://www.1905.com/search/index-p-type-film-q-{}.html?envod=1&year=0&score=0&order=0'.format(urllib.parse.quote(key))#只搜索能看电影,想搜其它的可以把html之后的字符删掉
 		#https://www.1905.com/search/index-p-type-all-q-{}.html
-		rsp = self.fetch(url, headers=self.header)
-		html = self.html(rsp.text)
-		aList = html.xpath("//div/div[contains(@class,'new_content')]/div/div/div/a")
-		videos = self.custom_list(aList=aList)
+		html = self.html(self.custom_webReadFile(url,self.header))
+		aList = html.xpath('//div[@class="main clearfix"]')
+		videos = self.custom_list_search(aList=aList)
 		result = {
 			'list':videos
 		}
@@ -173,8 +178,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 			signature = 'cid={0}&expiretime={1}&nonce={2}&page=https%3A%2F%2Fwww.1905.com%2Fvod%2Fplay%2F{3}.shtml&playerid={4}&type=hls&uuid={5}.dde3d61a0411511d'.format(id,expiretime,nonce,id,playerid,uid)
 			signature = hashlib.sha1(signature.encode()).hexdigest()
 			url = 'https://profile.m1905.com/mvod/getVideoinfo.php?nonce={0}&expiretime={1}&cid={2}&uuid={3}&playerid={4}&page=https%3A%2F%2Fwww.1905.com%2Fvod%2Fplay%2F{5}.shtml&type=hls&signature={6}&callback='.format(nonce,expiretime,id,uid,playerid,id,signature)
-			rsp = self.fetch(url, headers=self.header)
-			HtmlTxt=rsp.text
+			HtmlTxt=self.custom_webReadFile(url,self.header)
 			jo = json.loads(HtmlTxt.replace("(", "").replace(")", ""))
 			data = jo['data']['sign']
 			sign = ''
@@ -191,7 +195,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 			host = jo['data']['quality'][qualityStr]['host']
 			path = jo['data']['path'][qualityStr]['path']
 			playUrl = host + sign + path
-			result["parse"] = 1#0=直接播放、1=嗅探
+			result["parse"] = 0#0=直接播放、1=嗅探
 			result["playUrl"] =''
 			result["url"] = playUrl
 			result["header"] = self.header
@@ -202,25 +206,25 @@ class Spider(Spider):  # 元类 默认的元类 type
 		"player": {},
 		"filter": {
 		"n_1":[
-		{"key":"by","name":"排序:","value":[{"n":"默认(最新)","v":"o1"},{"n":"最热","v":"o3"},{"n":"好评","v":"o4"}]}
+		{"key":"by","name":"排序:","value":[{"n":"默认(最热)","v":"o3"},{"n":"最新","v":"o1"},{"n":"好评","v":"o4"}]}
 		],
 		"n_1_c_922":[
-		{"key":"by","name":"排序:","value":[{"n":"默认(最新)","v":"o1"},{"n":"最热","v":"o3"},{"n":"好评","v":"o4"}]}
+		{"key":"by","name":"排序:","value":[{"n":"默认(最热)","v":"o3"},{"n":"最新","v":"o1"},{"n":"好评","v":"o4"}]}
 		],
 		"n_2":[
-		{"key":"by","name":"排序:","value":[{"n":"默认(最新)","v":"o1"},{"n":"最热","v":"o3"},{"n":"好评","v":"o4"}]}
+		{"key":"by","name":"排序:","value":[{"n":"默认(最热)","v":"o3"},{"n":"最新","v":"o1"},{"n":"好评","v":"o4"}]}
 		],
 		"c_927":[
-		{"key":"by","name":"排序:","value":[{"n":"默认(最新)","v":"o1"},{"n":"最热","v":"o3"},{"n":"好评","v":"o4"}]}
+		{"key":"by","name":"排序:","value":[{"n":"默认(最热)","v":"o3"},{"n":"最新","v":"o1"},{"n":"好评","v":"o4"}]}
 		],
 		"n_1_c_586":[
-		{"key":"by","name":"排序:","value":[{"n":"默认(最新)","v":"o1"},{"n":"最热","v":"o3"},{"n":"好评","v":"o4"}]}
+		{"key":"by","name":"排序:","value":[{"n":"默认(最热)","v":"o3"},{"n":"最新","v":"o1"},{"n":"好评","v":"o4"}]}
 		],
 		"n_1_c_178":[
-		{"key":"by","name":"排序:","value":[{"n":"默认(最新)","v":"o1"},{"n":"最热","v":"o3"},{"n":"好评","v":"o4"}]}
+		{"key":"by","name":"排序:","value":[{"n":"默认(最热)","v":"o3"},{"n":"最新","v":"o1"},{"n":"好评","v":"o4"}]}
 		],
 		"n_1_c_1024":[
-		{"key":"by","name":"排序:","value":[{"n":"默认(最新)","v":"o1"},{"n":"最热","v":"o3"},{"n":"好评","v":"o4"}]}
+		{"key":"by","name":"排序:","value":[{"n":"默认(最热)","v":"o3"},{"n":"最新","v":"o1"},{"n":"好评","v":"o4"}]}
 		]					
 		}
 		}
@@ -241,11 +245,11 @@ class Spider(Spider):  # 元类 默认的元类 type
 			url=a.xpath("./@href")[0]
 			if url.find('vip.1905')>1:#可以除掉
 				continue
-			if self.regStr(reg=r'(play)',src=url)=='':
-				vod_id="{0}".format(url)
+			if self.custom_RegexGetText(url,'(play)',1)=='':
+				vod_id="{0}###{1}###{2}".format(url,title,img)
 			else:
-				id=self.regStr(reg=r'play/(.*?)\.sh',src=url)
-				vod_id="{0}".format(id)
+				id=self.custom_RegexGetText(url,r'play/(.*?)\.sh',1)
+				vod_id="{0}###{1}###{2}".format(id,title,img)
 			videos.append({
 					"vod_id":vod_id,
 					"vod_name":title,
@@ -253,3 +257,53 @@ class Spider(Spider):  # 元类 默认的元类 type
 					"vod_remarks":''
 				})
 		return videos
+	def custom_list_search(self,aList):
+		videos = []
+		for a in aList:
+			try:
+				img=a.xpath('./div[@class="movie-pic"]/a[@class="img-a"]/img/@src')[0]
+				title=a.xpath('./div[@class="movie-pic"]/a[@class="img-a"]/img/@alt')[0]
+				url=a.xpath('./ul[@class="cont"]/li[@class="spec paly-tab-icon position-icon"]/a/@href')
+				if len(url)<1:
+					url=a.xpath('./div[@class="movie-pic"]/a[@class="img-a"]/@href')[0]
+				else:
+					url=url[0]
+					if url.find('vip.1905')>1:
+						url=a.xpath('./div[@class="movie-pic"]/a[@class="img-a"]/@href')[0]
+				if self.custom_RegexGetText(url,'(play)',1)=='':
+					vod_id="{0}###{1}###{2}".format(url,title,img)
+				else:
+					id=self.custom_RegexGetText(url,r'play/(.*?)\.sh',1)
+					vod_id="{0}###{1}###{2}".format(id,title,img)
+				videos.append({
+						"vod_id":vod_id,
+						"vod_name":title,
+						"vod_pic":img,
+						"vod_remarks":''
+					})
+			except:
+				pass
+		return videos
+	#访问网页
+	def custom_webReadFile(self,urlStr,header=None,codeName='utf-8'):
+		html=''
+		if header==None:
+			header={
+				"Referer":urlStr,
+				'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36',
+				"Host":self.custom_RegexGetText(Text=urlStr,RegexText='https*://(.*?)(/|$)',Index=1)
+			}
+		req=urllib.request.Request(url=urlStr,headers=header)#,headers=header
+		with  urllib.request.urlopen(req)  as response:
+			html = response.read().decode(codeName,'ignore')
+		return html
+
+	#正则取文本
+	def custom_RegexGetText(self,Text,RegexText,Index):
+		returnTxt=""
+		Regex=re.search(RegexText, Text, re.M|re.S)
+		if Regex is None:
+			returnTxt=""
+		else:
+			returnTxt=Regex.group(Index)
+		return returnTxt
